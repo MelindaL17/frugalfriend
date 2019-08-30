32798afc8916531206bfc22a0b447ddbeedff552
MLnoobcoder <melindaling14@gmail.com>
2019-08-30 09:51:26 -0400

    styling for upload component

--- src/components/records/ImageUpload.js
+++ src/components/records/ImageUpload.js
@@ -5,7 +5,6 @@ import {uploadReceipt} from '../../store/actions/receiptActions'
 class Uploader extends Component {
   
   handleChange = (event) => {
-    console.log(event.target.value, 'TARGET')
     this.setState({
       [event.target.id]: event.target.files[0]
     })
@@ -16,13 +15,9 @@ class Uploader extends Component {
   }
   
   render () {
-    // console.log(this.state.image ? this.state.image.name : null)
+
     return (
-      // <div className="image-upload">
-      //   <input type="file" className="custom-file-input" id='image' onChange={this.handleChange}></input>
-      //   <button className="upload-button" onClick={this.handleUpload}>Upload</button>
-      // </div>
-      <div>
+      <div className="image-upload-container">
         <input type="file" className="inputfile" id='image' onChange={this.handleChange}></input>
         <label htmlFor="image">
           {!this.state ? <span>Choose a file....</span> : this.state.image.name }
