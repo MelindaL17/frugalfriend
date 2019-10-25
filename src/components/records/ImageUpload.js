9f8b44a56cc9dd280c4e843f2ecb0fe2b0834223
MelindaL17 <43175722+MelindaL17@users.noreply.github.com>
2019-10-25 10:43:26 -0400

    styled signin signout page (#7)

--- src/components/records/ImageUpload.js
+++ src/components/records/ImageUpload.js
@@ -1,44 +1,83 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import {uploadReceipt} from '../../store/actions/receiptActions'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { uploadReceipt } from "../../store/actions/receiptActions";
 
+const initialState = {
+  imageSelected:false,
+  errorMessage: ""
+}
 class Uploader extends Component {
-  
-  handleChange = (event) => {
-    this.setState({
-      [event.target.id]: event.target.files[0]
-    })
+  constructor() {
+    super();
+    this.state = initialState;
   }
-  handleUpload = async event => {
-    event.preventDefault()
-    this.props.uploadReceipt(this.state)
-  }
-  
-  render () {
 
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.files[0],
+      imageSelected: true,
+      errorMessage: ""
+    });
+    console.log("handlechange", event.target);
+  };
+  
+  handleUpload = event => {
+    event.preventDefault();
+    if(this.state.image) {
+      this.props.uploadReceipt(this.state);
+      this.setState({ 
+        imageSelected: false,
+        errorMessage: ''
+      });
+    } else {
+      this.setState({
+        errorMessage: "No receipt selected"
+      })
+    }
+    this.setState({
+      initialState
+    })
+  };
+
+  render() {
     return (
       <div className="image-upload-container">
-        <input type="file" className="inputfile" id='image' onChange={this.handleChange}></input>
+        <input
+          type="file"
+          className="inputfile"
+          id="image"
+          onChange={this.handleChange}
+        ></input>
         <label htmlFor="image">
-          {!this.state ? <span>Choose a file....</span> : this.state.image.name }
+          {!this.state.image ? (
+            <span>Choose a file....</span>
+          ) : (
+            this.state.image.name
+          )}
         </label>
-          <button className="upload-button" onClick={this.handleUpload}>Upload</button>
+        <button className="upload-button" onClick={this.handleUpload}>
+          Upload
+        </button>
+        <div>
+        {this.state.errorMessage ? <span className="errorMessage">{this.state.errorMessage}</span> : null}
+        </div>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     receipt: state.receiptReducer.allReceipts
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    uploadReceipt: (receipt) => dispatch(uploadReceipt(receipt))
-  }
-}
-export default 
-  connect(mapStateToProps, mapDispatchToProps)(Uploader)
-
+    uploadReceipt: receipt => dispatch(uploadReceipt(receipt))
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Uploader);
